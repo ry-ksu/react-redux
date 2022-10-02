8755194481418889b306e53acc1fbb7a1f71ff17
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-10-02 13:25:24 +0300

    feat: add search filter

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Cards } from '../cards';
 import { Search } from 'components/search';
-import { gameList } from '../gameList';
 
 export const Home = () => {
   return (
     <>
       <Search />
-      <Cards gameList={gameList} />
     </>
   );
 };
--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Cards } from '../cards';
+import { gameList } from '../gameList';
 
 type SearchState = {
   inputSearch: string;
@@ -11,6 +13,14 @@ export class Search extends Component<SearchProps, SearchState> {
     inputSearch: '',
   };
 
+  componentDidMount() {
+    this.setState({ inputSearch: JSON.parse(localStorage.getItem('search-value') || '') });
+  }
+
+  componentWillUnmount() {
+    localStorage.setItem('search-value', JSON.stringify(this.state.inputSearch));
+  }
+
   handleInputSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const {
       target: { value: inputSearch },
@@ -18,12 +28,32 @@ export class Search extends Component<SearchProps, SearchState> {
     this.setState({ inputSearch });
   };
 
+  filteredGames = (inputSearch: string) => {
+    if (inputSearch === '') {
+      return gameList;
+    }
+
+    const lowerCaseInputSearch = inputSearch.toLowerCase();
+    return gameList.filter((game) => {
+      return (
+        game.creator.toLowerCase().includes(lowerCaseInputSearch) ||
+        game.description.toLowerCase().includes(lowerCaseInputSearch) ||
+        game.rating.toString().includes(lowerCaseInputSearch) ||
+        game.title.toLowerCase().includes(lowerCaseInputSearch) ||
+        game.year.toString().includes(lowerCaseInputSearch)
+      );
+    });
+  };
+
   render() {
     const { inputSearch } = this.state;
     return (
-      <form>
-        <input type="search" value={inputSearch} onChange={this.handleInputSearch} />
-      </form>
+      <>
+        <form>
+          <input type="search" value={inputSearch} onChange={this.handleInputSearch} />
+        </form>
+        <Cards gameList={this.filteredGames(inputSearch)} />
+      </>
     );
   }
 }
