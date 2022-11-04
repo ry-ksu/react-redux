7d3f2dc70ad550ddd9c9be176d235ca15574be4e
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-11-04 18:02:33 +0300

    WIP

--- src/components/gameList/index.tsx
+++ src/components/gameList/index.tsx
@@ -30,12 +30,16 @@ export const GameList = (prop: IGameListProps) => {
   }
 
   return (
-    <div className={styles.cardsWrapper}>
-      <div className={styles.gamesCards}>
-        <GameList__header />
-        {content}
+    <div className={styles['game-list']}>
+      <div className={styles['game-list-wrapper']}>
+        <div className={styles['game-list__cards']}>
+          <div className={styles['game-list__header']}>
+            <GameList__header />
+          </div>
+          <div className={styles['game-list__content']}>{content}</div>
+        </div>
+        <Loader isLoading={gamesState.isLoaded} />
       </div>
-      <Loader isLoading={gamesState.isLoaded} />
     </div>
   );
 };
